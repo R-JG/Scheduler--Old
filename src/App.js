04fd89c1a80412f83c2c29eae755f7289d3e72fd
Robin <robin.james.guerrier@gmail.com>
2022-12-15 23:31:05 -0500

    Add generateDates function which returns an array of 35 dates beginning on Sunday of the week in which the month begins.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+    const [ currentDate, setCurrentDate ] = useState(new Date()); 
+    const [ calendarDates, setCalendarDates ] = useState(
+        generateDates(currentDate.getFullYear(), currentDate.getMonth())
+    );
+
+    function generateDates(year, month) {
+        const dateArray = [];
+        const dayMonthBegins = new Date(year, month, 1).getDay();
+        for (let i = 0; i < 35; i++) {
+            let dayValue = i - dayMonthBegins;
+            if (i >= dayMonthBegins) {
+                dayValue++;
+            };
+            dateArray.push(new Date(year, month, dayValue));
+        };
+        return dateArray;
+    };
+
+    // events will be updated independently in state, saved in local storage, and rendered upon the condition that the date object corresponds with the date saved with the event. 
+
+    console.log(calendarDates);
+
+    return (
+        <main>
+            <div className='calendar-container'></div>
+        </main>
+    );
+};
